2903aa5d25edf1c3ddbea081d0b926e5dae4fb71
qiubinglin <592559150@qq.com>
2021-04-22 09:51:35 +0800

    add get_circumsphere

--- delaunay/Delaunay2D.rs
+++ delaunay/Delaunay2D.rs
@@ -1,7 +1,7 @@
 struct Point {
     x: f64,
     y: f64,
-    // z: f64,
+    z: f64,
 }
 
 impl Point {
@@ -11,29 +11,41 @@ impl Point {
 }
 
 struct Edge {
-    p1: Point,
-    p2: Point,
+    p0: i32,
+    p1: i32
 }
 
 struct Triangle {
-    p1: Point,
-    p2: Point,
-    p3: Point,
+    p0: i32,
+    p1: i32,
+    p2: i32,
     center: Point,
     radius_sqr: f64,
 }
 
-impl Triangle {
-    pub fn new(p1: &Point, p2: &Point, p3: &Point) -> Triangle {
-        self {
-            p1: p1,
-            p2: p2,
-            p3: p3,
-            center: {
+fn get_circumsphere(p0: &Point, p1: &Point, p2: &Point, center: &mut Point, radius_sqr: &mut f64) {
+    x1 = p1.x - p0.x;
+    y1 = p1.y - p0.y;
+    x2 = p2.x - p0.x;
+    y2 = p2.y - p0.y;
 
-            }
-        }
+    // calc cross
+    det = x1 * y2 - y1 * x2;
+    if det == 0i64 {
+        center = p0;
+        radius_sqr = 0i64;
+        return;
     }
+    
+    det = 0.5i64 /det;
+    len1 = x1*x1 + y1*y1;
+    len2 = x2*x2 + y2*y2;
+    cx = (len1 * y2 - len2 * y1) * det
+    cy = (len2 * x1 - len1 * x2) * det
+    center.x = p0.x + cx;
+	center.y = p0.y + cy;
+	center.z = 0i64;
+	radius_sqr = cx * cx + cy * cy;
 }
 
 struct Delaunay2D {
