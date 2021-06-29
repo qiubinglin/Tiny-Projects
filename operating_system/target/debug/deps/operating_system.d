51047e4326693df3170901b29e753088ad40e036
qiubinglin <qiubinglin@outlook.com>
2021-06-29 22:18:17 +0800

    Begin freestanding os

---
+++ operating_system/target/debug/deps/operating_system.d
@@ -0,0 +1,5 @@
+E:\workspace\Tiny-Projects\operating_system\target\debug\deps\operating_system.exe: src\main.rs
+
+E:\workspace\Tiny-Projects\operating_system\target\debug\deps\operating_system.d: src\main.rs
+
+src\main.rs:
