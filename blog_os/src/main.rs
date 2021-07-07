2e0553f2012234475b74f519d8a958fe16ab65af
qiubinglin <592559150@qq.com>
2021-07-07 15:12:29 +0800

    study blog_os

--- blog_os/src/main.rs
+++ blog_os/src/main.rs
@@ -12,9 +12,7 @@ pub extern "C" fn _start() -> ! {
     // this function is the entry point, since the linker looks for a function
     // named `_start` by default
 
-    use core::fmt::Write;
-    vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
-    write!(vga_buffer::WRITER.lock(), ", some numbers: {} {}", 42, 1.337).unwrap();
+    println!("Hello World{}", "!");
 
     loop {}
 }
